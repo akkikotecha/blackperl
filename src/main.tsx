74f6df2d0e8698a6b66fda8ef3b0990fe2b89024
akkikotecha <akshaykotecha1999@gmail.com>
2024-06-26 16:40:25 +0530

    main ts file change

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
