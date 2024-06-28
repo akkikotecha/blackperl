d39f79cbcf5463290d81d8cb91c5047261b96f2b
akkikotecha <akshaykotecha1999@gmail.com>
2024-06-28 14:53:05 +0530

    pdf download

--- src/Page/bccd.tsx
+++ src/Page/bccd.tsx
@@ -74,7 +74,7 @@ import { useEffect } from "react";
           </h1>
           </div>
           <div className={`${styles.flex_row} ${styles['col-2']}`}>
-        <a href="./../../public/collbrator/BPCD.pdf" download> <button className={`${styles.am} ${styles.ammm}`}>
+        <a href="./../../collbrator/BPCD.pdf" download> <button className={`${styles.am} ${styles.ammm}`}>
             <b className={styles.contactUs}>View More</b>
             <img className={styles.amChild} alt="" src="/group-457.svg" />
           </button></a>
